ff2ed9fa3600c4a5d723f528f6fb277d2c288b01
Avery Keuben <avery1516@gmail.com>
2025-03-15 15:14:35 -0600

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+    description = "GEARS Game";
+    inputs.flake-utils.url = "github:numtide/flake-utils";
+    outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in {
+            devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+    );
+}
